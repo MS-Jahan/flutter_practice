79607563321fc0974b6343479754e901e9f1ebe9
ms-jahan <ssarwarjahan@gmail.com>
2022-09-05 20:20:36 +0600

    container margin padding

--- from_Soykot_Hosen/first_app/lib/main.dart
+++ from_Soykot_Hosen/first_app/lib/main.dart
@@ -11,14 +11,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.white,
-            body: Center(
-              child: Icon(
-                Icons.android,
-                color: Colors.green,
-                size: 200.0,
-              ),
-            )));
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          child: Text(
+            "Container",
+            style: TextStyle(
+              fontSize: 30.0,
+              color: Colors.blue,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: Colors.pink,
+            shape: BoxShape.circle,
+          ),
+          //margin: EdgeInsets.all(50.0),
+          // padding: EdgeInsets.all(80.0),
+          alignment: Alignment.center,
+          height: 250.0,
+          width: 250.0,
+        ),
+      ),
+    );
   }
 }
