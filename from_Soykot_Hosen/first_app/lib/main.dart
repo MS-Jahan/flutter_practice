d9c17ab6caf1973351ea9eb7653a69cce6eca370
ms-jahan <ssarwarjahan@gmail.com>
2022-09-07 09:40:07 +0600

    StatefulWidget, InkWell and GestureDetector

--- from_Soykot_Hosen/first_app/lib/main.dart
+++ from_Soykot_Hosen/first_app/lib/main.dart
@@ -1,13 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(MyApp());
+  return runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+int clickNumber = 0;
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,67 +22,27 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             Expanded(
-              flex: 1,
-              child: Container(
-                child: Text(
-                  "1",
-                  style: TextStyle(
-                    fontSize: 36.0,
-                    color: Colors.white,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    clickNumber += 1;
+                    print("Press Container");
+                    print("Click number is = + $clickNumber");
+                  });
+                },
+                child: Container(
+                  color: Colors.green,
+                  alignment: Alignment.center,
+                  child: Text(
+                    clickNumber.toString(),
+                    style: const TextStyle(
+                      fontSize: 36.0,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-                color: Colors.green,
-                alignment: Alignment.center,
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                child: Text(
-                  "2",
-                  style: TextStyle(
-                    fontSize: 36.0,
-                    color: Colors.white,
-                  ),
-                ),
-                color: Colors.red,
-                alignment: Alignment.center,
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                child: Text(
-                  "3",
-                  style: TextStyle(
-                    fontSize: 36.0,
-                    color: Colors.white,
-                  ),
-                ),
-                color: Colors.green,
-                alignment: Alignment.center,
-              ),
-            ),
-
-            //   child: Text(
-            //     "Container 3",
-            //     style: TextStyle(
-            //       fontSize: 20.0,
-            //       color: Colors.blue,
-            //       fontStyle: FontStyle.italic,
-            //       fontWeight: FontWeight.bold,
-            //     ),
-            //   ),
-            //   decoration: BoxDecoration(
-            //     color: Colors.pink,
-            //     shape: BoxShape.rectangle,
-            //   ),
-            //   //margin: EdgeInsets.all(50.0),
-            //   // padding: EdgeInsets.all(80.0),
-            //   alignment: Alignment.center,
-            //   height: 180.0,
-            //   width: 180.0,
-            // ),
           ],
         ),
       ),
